05d529d95fddde41caa60d55508f9e89757209f4
lucaidau <dotoan270806@gmail.com>
2026-01-13 02:54:12 +0700

    Description of css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      color: {
+      colors: {
+        //main background
         "primary-bg": "#0F172A",
         "secondary-bg": "#0F172A",
-
+        //main text
         "primary-txt": "#E5E7EB",
         "secondary-txt": "#9CA3AF",
         "blur-txt": "#6B7280",
-
+        //Effect
         "primary-accent": "#3B82F6",
         "soft-accent": "#2563EB",
         "hover-active": "#60A5FA",
-
+        //Border
         "primary-border": "#60A5FA",
         "light-divider": "#374151",
-
+        //Navbar
         "navbar-bg": "#374151",
         "navbar-txt": "#9CA3AF",
         "navbar-hover": "#E5E7EB",
