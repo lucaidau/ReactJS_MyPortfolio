ea8d850f1ff92131a55c9f34d34529e6cf57878c
lucaidau <dotoan270806@gmail.com>
2026-01-13 01:36:58 +0700

    Set up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import About from "./pages/about";
+import Error404 from "./components/Error404";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <nav className="flex gap-4">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/projects"}>Projects</Link>
+          <Link to={"/contact"}>Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/projects" element={<Projects></Projects>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="*" element={<Error404></Error404>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        "primary-bg": "#0F172A",
+        "secondary-bg": "#0F172A",
+
+        "primary-txt": "#E5E7EB",
+        "secondary-txt": "#9CA3AF",
+        "blur-txt": "#6B7280",
+
+        "primary-accent": "#3B82F6",
+        "soft-accent": "#2563EB",
+        "hover-active": "#60A5FA",
+
+        "primary-border": "#60A5FA",
+        "light-divider": "#374151",
+
+        "navbar-bg": "#374151",
+        "navbar-txt": "#9CA3AF",
+        "navbar-hover": "#E5E7EB",
+        "navbar-txt-active": "#3B82F6",
+        "navbar-underline": "#3B82F6",
+      },
+    },
   },
   plugins: [],
 };
