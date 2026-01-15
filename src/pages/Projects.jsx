fc20034c734f619651dfcabb0516518426dabc31
lucaidau <dotoan270806@gmail.com>
2026-01-16 03:46:05 +0700

    Add projects data

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
+const projectData = [
+  {
+    title: "Weather App",
+    description: "",
+    tech: ["React", "Tailwind", "Vite", "Restful API"],
+    github: "#",
+    link: "",
+    image: "",
+  },
+  {
+    title: "",
+    description: "",
+    tech: [],
+    github: "#",
+    link: "",
+    image: "",
+  },
+  {
+    title: "",
+    description: "",
+    tech: [],
+    github: "#",
+    link: "",
+    image: "",
+  },
+];
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      {projectData.map((pj, indx) => (
+        <div key={indx}>
+          <h3>{pj.title}</h3>
+          <p>{pj.description}</p>
+
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
